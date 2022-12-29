d09c20c7d867a2bbabd8668580b5c1731bbedd6c
Mateusz Kubis <mkubis98@gmail.com>
2022-12-29 15:48:17 +0100

    code-cleanup

--- src/api/ApiGlobal.ts
+++ src/api/ApiGlobal.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+export const apiConfig = {
+      headers: { Authorization: `${localStorage.getItem('token')}` }
+    };
+
 export default axios.create({
   baseURL: `http://192.168.13.14:5000/`,
 });
